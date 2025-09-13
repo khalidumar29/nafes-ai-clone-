f7db461a00a2a3ce89ac32801d350f9d4f13f53f
basar <abulbasarofficial5403@gmail.com>
2025-09-13 20:02:33 +0600

    add terms and conditions page

---
+++ src/app/(frontend)/[locale]/terms-and-conditions/page.tsx
@@ -0,0 +1,233 @@
+const Page = () => {
+  return (
+    <main className="container text-[#212529] text-sm py-10">
+      <p>Nafes Platform Terms and Conditions</p>
+      <p>
+        The Customer acknowledges and agree that accessing Nafes Platform/ website and using its
+        services will be subject to the below Terms and Conditions.
+      </p>
+      <p>1. Subscriptions</p>
+      <p>
+        1.1 The Company shall sell to the Customer the number of subscriptions specified in the
+        Order Form, enabling the Customer and its employees to access and benefit from all services
+        available on the Nafes Platform.
+      </p>
+      <p>
+        1.2 Nafes Account: The Customer will be required to create an account to access the services
+        (“Nafes Account”). To properly create a Nafes Account, the Customer must: Ensure that the
+        Nafes Account is created by authorized personnel of the Customer and that all required
+        information is provided to meet the subscription requirements; and Provide the Company with
+        true, accurate, and complete information about the Customer and maintain the accuracy and
+        completeness of this information at all times.
+      </p>
+      <p>2. Nafes Responsibilities</p>
+      <p>Nafes shall:</p>
+      <p>
+        Provide services to the Customer in accordance with the applicable laws of the Kingdom of
+        Saudi Arabia, these Terms and Conditions, and the Order Form;
+      </p>
+      <p>
+        During the Subscription Term, use commercially reasonable efforts to ensure the continuous
+        operation of the Nafes Platform as outlined in this Terms and Conditions. The Platform may
+        be temporarily unavailable due to scheduled maintenance, unscheduled emergency maintenance
+        by the Company or third-party providers, or due to causes beyond the Company’s reasonable
+        direct control. However, the Company will use reasonable efforts to notify the Customer in
+        advance via email of any scheduled downtime;
+      </p>
+      <p>
+        Provide the Nafes Platform on an “as is” and “as available” basis, without any warranties or
+        representations of any kind, whether express or implied, except as explicitly stated in
+        these Terms and Conditions;
+      </p>
+      <p>
+        Not guarantee that the Nafes Platform is free of viruses or other harmful components, or
+        that it will be uninterrupted or error-free, or that its features will operate in
+        conjunction with other software, hardware, or systems, or be immune to hacking or
+        unauthorized access by third parties; and
+      </p>
+      <p>
+        Use its best commercial efforts to provide support to the Customer as outlined in these
+        Terms and Conditions and the Order Form.
+      </p>
+      <p>3. Customer Responsibilities</p>
+      <p>The Customer shall:</p>
+      <p>Pay the subscription fees as specified in the Order Form;</p>
+      <p>
+        Acknowledge that Nafes merely aggregates and displays tenders and competitions announced in
+        the Kingdom of Saudi Arabia as received from their respective sources and does not provide
+        any technical and/or professional advice regarding any tender/competition listed on the
+        Nafes Platform;
+      </p>
+      <p>
+        Acknowledge that the AI-powered services available on the Nafes Platform are intended solely
+        to assist and guide the Customer and its employees in locating and analyzing
+        tenders/competitions and are not to be construed as technical and/or professional advice.
+        The Customer agrees not to rely on the analyses or outputs from the Platform for making any
+        decisions related to such tenders or competitions;
+      </p>
+      <p>
+        Use the Nafes Platform solely for the purpose of reviewing and evaluating
+        tenders/competitions announced in Saudi Arabia and shall not engage in reverse engineering,
+        decompiling, disassembling, translating, or attempting to discover the source code or
+        underlying ideas of the software, nor copy or reproduce any content or use it to create
+        competing products or services;
+      </p>
+      <p>
+        Not replicate, modify, publish, transmit, distribute, or otherwise exploit any part of the
+        Nafes Platform by any means;
+      </p>
+      <p>
+        Not access the Nafes Platform for the purpose of developing or offering a competing platform
+        or service;
+      </p>
+      <p>
+        Be solely responsible for its use of the services and for any access to the Nafes Platform;
+      </p>
+      <p>
+        Not transfer, sublicense, lease, resell, or make the Platform accessible, even temporarily,
+        to unauthorized users or third parties without prior written consent from the Company;
+      </p>
+      <p>
+        Use commercially reasonable efforts to prevent unauthorized access and promptly notify Nafes
+        of any unauthorized use or access;
+      </p>
+      <p>
+        Acknowledge that the Nafes Platform is provided “as is” and that the Company shall not be
+        liable for the Customer's or its employees' inability to access or use the Platform due to
+        circumstances beyond the Company’s direct and full control, including but not limited to
+        those involving internet service or telecom providers.
+      </p>
+      <p>4. Term, Termination, and Suspension</p>
+      <p>
+        Term: The subscription term is one (1) calendar year starting from the Effective Date
+        specified in the Order Form (“Subscription Term”) and will automatically renew for
+        successive terms unless either party notifies the other in writing of its intent not to
+        renew at least two (2) months prior to the expiration of the then-current term.
+      </p>
+      <p>
+        Termination for Material Breach: Either party may terminate the Subscription Term upon
+        written notice if the other party materially breaches this Terms and Conditions and fails to
+        cure such breach within ten (10) business days of receiving written notice thereof.
+      </p>
+      <p>
+        Termination for Insolvency: Either party may immediately terminate the Subscription Term by
+        written notice in the event the other party is liquidated, dissolved, files for bankruptcy,
+        makes an assignment for the benefit of creditors, or ceases business operations.
+      </p>
+      <p>
+        Termination for Unlawful Use of Nafes Platform: The Company may terminate the Subscription
+        immediately without notice if the Client uses the Platform or services for any unlawful or
+        prohibited purpose, including but not limited to unauthorized use of source code or any
+        attempt to harm, disrupt, or compete with the Company or the Nafes Platform.
+      </p>
+      <p>
+        Subscription Suspension: The Company may suspend the Customer’s access to the Nafes Platform
+        immediately upon the Customer’s failure to pay any due amounts, breach of any part of this
+        Terms and Conditions, or involvement in unlawful activities that may harm the Company.
+      </p>
+      <p>
+        Effect of Termination or Suspension: Termination or suspension does not relieve the Client
+        of its obligations accrued up to the effective date thereof. Upon such termination or
+        suspension:
+      </p>
+      <p>All rights and subscriptions granted to the Client shall immediately cease;</p>
+      <p>
+        The Client must pay all outstanding dues within forty-eight (48) hours, and acknowledges
+        that any fees already paid are non-refundable.
+      </p>
+      <p>5 Fees and Payment Terms</p>
+      <p>
+        5.1 Fees: The Client agrees to pay the subscription fees specified in the Order Form
+        (“Fees”).
+      </p>
+      <p>
+        5.2 Taxes: Fees are exclusive of any applicable taxes, including VAT and other
+        government-imposed charges, which shall be borne by the Client.
+      </p>
+      <p>
+        5.3 Payment Terms: Unless otherwise stated in the Order Form, all fees are payable in
+        advance in a single lump sum upon signing the Order Form.
+      </p>
+      <p>
+        5.4 Fee Adjustment: The fees stated are fixed during the initial Subscription Term. The
+        Company may revise fees for any renewal term by providing written notice at least one (1)
+        month before the current term expires. The Client may accept the new fees or notify the
+        Company at least fourteen (14) days prior to the renewal date if it chooses not to renew.
+      </p>
+      <p>6 Force Majeure</p>
+      <p>
+        Neither party shall be liable for any delay or failure in performance due to a force majeure
+        event. Each party must notify the other promptly upon becoming aware of such an event. If
+        the event continues for more than sixty (60) days, either party may terminate this Terms and
+        Conditions without further liability, except for accrued obligations.
+      </p>
+      <p>7 Intellectual Property</p>
+      <p>
+        7.1 The Customer acknowledges and agrees that all intellectual property rights in the Nafes
+        Platform and services belong solely to the Company or its licensors. No rights are granted
+        to the Client other than as explicitly stated herein.
+      </p>
+      <p>
+        7.2 The Customer agrees to indemnify the Company, its licensors, and affiliates against all
+        claims, damages, or costs resulting from the infringement or alleged infringement of
+        intellectual property rights arising from the Customer’s or its users’ use of the Nafes
+        Platform.
+      </p>
+      <br />
+      <p>8 Indemnification and Limitation of Liability</p>
+      <p>
+        8.1 To the extent permitted by applicable law in Saudi Arabia, the Company shall not be
+        liable for:
+      </p>
+      <p>
+        Any use of the Nafes Platform in violation of this Terms and Conditions or local laws; or
+      </p>
+      <p>Integration of the Platform with unauthorized applications or systems.</p>
+      <p>
+        8.2 The Company shall not be liable for any direct or indirect losses, reputational damage,
+        incidental or consequential damages, lost profits or opportunities, or expenses resulting
+        from the use or reliance on the Platform.
+      </p>
+      <p>
+        8.3 The Company shall also not be liable for losses arising from force majeure or
+        third-party actions.
+      </p>
+      <p>
+        8.4 The parties agree that the Company's total liability shall not exceed the total
+        subscription fees actually received from the Customer at the time of the claim.
+      </p>
+      <p>
+        8.5 The Customer shall indemnify and hold harmless the Company, its owners, affiliates,
+        employees, and agents from any liability, claims, damages, reputational loss, legal fees,
+        and other costs arising from any unauthorized use of the Platform or breach of this Terms
+        and Conditions.
+      </p>
+      <br />
+      <p>9 General Provisions</p>
+      <p>
+        9.1 Assignment: Neither party may assign or transfer its rights or obligations without the
+        other party’s prior written consent.
+      </p>
+      <br />
+      <br />
+      <p>
+        9.2 Severability: If any provision is found to be invalid or unenforceable, the remainder of
+        the Terms and Conditions shall remain in full force.
+      </p>
+      <br />
+      <br />
+      <p>
+        9.3 Notices: All notices must be in writing and delivered via email to the addresses
+        specified in the Order Form.
+      </p>
+      <br />
+      <p>
+        9.4 Governing Law and Jurisdiction: This Terms and Conditions shall be governed by the laws
+        of the Kingdom of Saudi Arabia, and the courts of Riyadh shall have exclusive jurisdiction
+        over any disputes arising hereunder.
+      </p>
+    </main>
+  )
+}
+
+export default Page
