b5d3c1f5a9704f4d5f78524f9c65d92949d6b0ea
basar <abulbasarofficial5403@gmail.com>
2025-09-16 02:25:24 +0600

    add light

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -6,15 +6,13 @@ import { GeistSans } from 'geist/font/sans'
 import React from 'react'
 
 import { Providers } from '@/providers'
-import { InitTheme } from '@/providers/Theme/InitTheme'
 import { mergeOpenGraph } from '@/utilities/mergeOpenGraph'
-import { draftMode } from 'next/headers'
 
 import Footer from '@/components/shared/Footer'
 import Navbar from '@/components/shared/Navbar'
 import { getServerSideURL } from '@/utilities/getURL'
-import { getPayload } from 'payload'
 import config from '@payload-config'
+import { getPayload } from 'payload'
 import 'swiper/css'
 import 'swiper/css/bundle'
 import './globals.css'
@@ -24,9 +22,14 @@ export default async function RootLayout({ children }: { children: React.ReactNo
   const navbar = await payload.findGlobal({ slug: 'navbar' as any })
   const footer = await payload.findGlobal({ slug: 'footer' })
   return (
-    <html className={cn(GeistSans.variable, GeistMono.variable)} lang="en" suppressHydrationWarning>
+    <html
+      className={cn(GeistSans.variable, GeistMono.variable)}
+      lang="en"
+      data-theme="light"
+      suppressHydrationWarning
+    >
       <head>
-        <InitTheme />
+        {/* <InitTheme /> */}
         <link href="/favicon.ico" rel="icon" sizes="32x32" />
         <link href="/favicon.svg" rel="icon" type="image/svg+xml" />
       </head>
