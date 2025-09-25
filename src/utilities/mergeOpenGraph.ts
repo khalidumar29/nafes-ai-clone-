8c56349644f371a0e6a470401d716b14df9965fe
Muhammad Khalid Umar <dreamiwoarld.online@gmail.com>
2025-09-26 02:01:33 +0600

    update meta image

--- src/utilities/mergeOpenGraph.ts
+++ src/utilities/mergeOpenGraph.ts
@@ -9,7 +9,7 @@ const defaultOpenGraph: Metadata['openGraph'] = {
     'Ostool is a smart cloud-based system that helps you manage your vehicle fleet through an easy-to-use interface that supports the Arabic language. You can track vehicles in real time, manage bookings and maintenance, organize customer and employee data, and monitor accounts and revenues. Ostool provides fully customizable fleet management tailored to your business needs, with strong security measures to ensure the safety of your data.',
   images: [
     {
-      url: `${getServerSideURL()}/website-template-OG.png`,
+      url: `${getServerSideURL()}/website-template-OG.webp`,
     },
   ],
 }
