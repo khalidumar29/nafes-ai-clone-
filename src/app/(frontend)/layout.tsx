468bc5a68a7a6c851b8a23916fc690ef9ef8d506
Muhammad Khalid Umar <dreamiwoarld.online@gmail.com>
2025-10-07 21:18:39 +0600

    favicaon icon update

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -21,8 +21,8 @@ export default async function RootLayout({ children }: { children: React.ReactNo
     >
       <head>
         {/* <InitTheme /> */}
-        <link href="/favicon.ico" rel="icon" sizes="32x32" />
-        <link href="/favicon.svg" rel="icon" type="image/svg+xml" />
+        <link href="/Favicon.png" rel="icon" sizes="32x32" />
+        <link href="/Favicon.svg" rel="icon" type="image/svg+xml" />
       </head>
       <body className="bg-white text-black">
         {/* <AdminBar
